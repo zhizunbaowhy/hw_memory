0d0a826e935aa1a4f55cc94fe8b581e322c3a05f
Pigeon.GuGuJi <Pigeon.GuGuJi@outlook.com>
2023-03-12 13:07:41 +0800

    增加了对于readfile和ls的测试

--- newCFG/test.py
+++ newCFG/test.py
@@ -2,11 +2,17 @@ from readfile import ASMFileReader
 from loadstore import LoadStore
 
 testfileobj = ASMFileReader("/Users/gugujixiao/workspace/project/HWMemory/Code/HW-Memory/example/old_example/func.asm")
+tempString = testfileobj.statements_table
+
+for i in tempString:
+    print(i)
 
 testls = LoadStore(testfileobj)
 
-temp = testls.test_func()
+#temp = testls.test_func()
+#for i in temp:
+#    print(i)
 
-temp2 = testls.processAddress(temp[1][2])
+#temp2 = testls.processAddress(temp[0][2])
 
-print(temp2)
+#print(temp2)
