c34de04f84bcf4036e502aabe7489629cce92240
Pigeon.GuGuJi <Pigeon.GuGuJi@outlook.com>
2023-03-14 20:24:26 +0800

    cfg_gen由原来的CFG改过来，让cfg_pre单独运行，单独决定是否调用gen进行画图

--- newCFG/cfg_gen.py
+++ newCFG/cfg_gen.py
@@ -1,13 +1,15 @@
 from graphviz import Digraph
 
-from cfg_preparation  import CFGNode_and_edge
-
 
 class cfg_gen:
+    
     def __init__(self,cfg_pre):
+
         self.isa=cfg_pre
         self.draw_cfg()
+    
     def draw_cfg(self):
+
         self.isa.CFGNode_and_edge_gen()
         dot = Digraph('single_block')
         for i in range(len(self.isa.head)):
@@ -16,6 +18,7 @@ class cfg_gen:
 
             dot.edge(self.isa.head[i], self.isa.tail[i], self.isa.order[i])
         dot.view()  # 画图
+
     #TODO:要把loop移植过来
 
 
