7072f03d03ad8782397c898d8546d5a31acac8ef
刘金源 <11691240+liu-jinyuan111@user.noreply.gitee.com>
2023-03-20 15:43:48 +0000

    update newCFG/test.py.
    Signed-off-by: 刘金源 <11691240+liu-jinyuan111@user.noreply.gitee.com>

--- newCFG/test.py
+++ newCFG/test.py
@@ -4,22 +4,32 @@ from loadstore import LoadStore
 from cfg_gen import cfg_gen
 from cfg_preparation import CFGNode_and_edge
 
+
 #测试readfile
-testFileObj = ASMFileReader("/Users/gugujixiao/workspace/project/HWMemory/Code/HW-Memory/example/old_example/func.asm")
-stat_line =testFileObj.statements_table
-stat_dtl = testFileObj.find_addr('400608')
+testFileObj = ASMFileReader(r"C:\Users\13377\Desktop\hw-memory-master\example\old_example\func.asm")
+#stat_line =testFileObj.statements_table
+
 #for i in stat_line:
 #    print(i)
 
+
+#print(testFileObj.statements_table[131][1][3].split()[1][1:-1])
 #测试CFG
-testCFGObj = CFGNode_and_edge(testFileObj)
-testCFGObj.CFGNode_and_edge_gen()
-testCFGObj.build_dfs()
+testCFGPre = CFGNode_and_edge(testFileObj)
+
+testCFGPre.CFGNode_and_edge_gen()
+draw=cfg_gen(testCFGPre)#  到这里就可以生成cfg了
+#以下我们进行loop的运行
+draw.gen_graph_shoot()
+draw.dfs(draw.shoot_graph,[],draw.shoot[testCFGPre.head[0]])  #深搜  #找出起点对应的数字，这里的起点尽量定义为head的第一个点，这样不会遇到从中间开始找不到loop的问题
+print(draw.loop)
+#testBlockInfo = testCFGPre.block_info
+#testCFGPre.build_block_table()
+
 
 #测试loadstore
-testls = LoadStore(testFileObj,testCFGObj)
+#testls = LoadStore(testFileObj)
 
-'''res = testls.block_store_table
-for i in res:
-    print(i)'''
+#res = testls.test_func(5)
 
+#print(res)
