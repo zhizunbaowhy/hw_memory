5ba17ba2864bd7227648ccba33d8ff6f25940703
Pigeon.GuGuJi <Pigeon.GuGuJi@outlook.com>
2023-03-23 13:38:09 +0800

    设置了类别的instr

--- newCFG/instr.py
+++ newCFG/instr.py
@@ -1,71 +1,125 @@
 import re
+from enum import Enum, auto
 from typing import Tuple, Optional
 
+# 标记语句类型用的枚举类
+class AddressMode(Enum):
+    
+    immOffset = auto()
+    regOffset = auto()
+    regShift = auto()
+    immBef = auto()
+    regBef = auto()
+    regShiftBef = auto()
+    immAfter  = auto()
+    regAfter  = auto()
+    regShiftAfter  = auto()
 
-# 正则表达式
-class ins_type:
+# 标记语句类型用的枚举类
+class InsType(Enum):
+    
+    branch = auto()
+    load = auto()
+    store = auto()
+    adr = auto()
+    mov = auto()
+    other = auto()
+
+class Re_Ins_Type:
     
     # 区分指令类型
     branch_pat = r"b|b.|bl|bc."
     load_pat = r"ldr|ldp|lda|ldu" 
     store_pat = r"str|stp|stl|stu"
+    lsp_pat = r"ldp|stp"
     adrp_pat = r"adrp"
     move_pat = r"mov"
 
-class operand_type:
+class RE_Operand_Type:
     #branch，看上去和adrp一样
     operand_branch_access_pat = r"([0-9a-fA-F]*)\s*(<.*>)"
     #adrp
     operand_adrp_access_pat = r"([0-9a-fA-F]*)\s*(<.*>)"
-    #带括号的
-    operand_bracket_pat = r"\[.*\]!?"
-    operand_bracket_sp_pat = r"\[[\s\S]*sp[\s\S]*\]!?"
-    operand_bracket_simpleReg_pat = r"\[((?:x|w)\d*)\]!?"
-    operand_bracket_regOffset_pat = r"\[((?:x|w)\d*)\s*\,\s*\#((?:\d*|0x[0-9a-fA-F]*))\]!?"
-    operand_bracket_regdotreg_pat = r"\[((?:x|w)\d*)\s*\,\s*((?:x|w)\d*)\]!?"
-    operand_bracket_regplusreg_pat = r"\[((?:x|w)\d*)\s*\+\s*((?:x|w)\d*)\]!?"
+    #ls判断流程
+    #特殊情况
+    ls_bracket_pat = r"\[.*\]"
+    ls_bracketUpdate_pat = r"\[.*\]!"
+    ls_bracket_sp_pat = r"\[[\s\S]*sp[\s\S]*\]"
+    ls_bracket_reg_pat = r"(\[((?:x|w)\d*)\])"
+    ls_sp_pat = r"(sp)"
+    ls_immOffset_pat = r"\#(?:\d*|0x[0-9a-fA-F]*)"
+    ls_reg_pat = r"((?:x|w)\d*)"
+    ls_shift_pat = r"(LSL|LSR|ASR|ROR)"
+    #偏移寻址
+    ls_immeOffset_pat = r"\[((?:x|w)\d*)\s*\,\s*\#(-?)((?:\d*|0x[0-9a-fA-F]*))\]"
+    ls_regOffset_pat = r"\[((?:x|w)\d*)\s*\,\s*(-?)((?:x|w)\d*)\]"
+    ls_regShift_pat = r"\[((?:x|w)\d*)\s*\,\s*(-?)((?:x|w)\d*)\s*\,\s*(LSL|LSR|ASR|ROR)\s*\#(-?)((?:\d*|0x[0-9a-fA-F]*))\]"
+    #先更新寻址
+    ls_immeBef_pat = r"\[((?:x|w)\d*)\s*\,\s*\#(-?)((?:\d*|0x[0-9a-fA-F]*))\]!"
+    ls_regBef_pat = r"\[((?:x|w)\d*)\s*\,\s*(-?)((?:x|w)\d*)\]!"
+    ls_regShiftBef_pat = r"\[((?:x|w)\d*)\s*\,\s*(-?)((?:x|w)\d*)\s*\,\s*(LSL|LSR|ASR|ROR)\s*\#(-?)((?:\d*|0x[0-9a-fA-F]*))\]!"
+
 
 class isa:
     #编译指令
-    __branch_cpat = re.compile(ins_type.branch_pat)
-    __load_cpat = re.compile(ins_type.load_pat)
-    __store_cpat = re.compile(ins_type.store_pat)
-    __adrp_cpat = re.compile(ins_type.adrp_pat)
-    __move_cpat = re.compile(ins_type.move_pat)
+    __branch_cpat = re.compile(Re_Ins_Type.branch_pat)
+
+    __load_cpat = re.compile(Re_Ins_Type.load_pat)
+    __store_cpat = re.compile(Re_Ins_Type.store_pat)
+    __lsp_cpat = re.compile(Re_Ins_Type.lsp_pat)
+
+    __adrp_cpat = re.compile(Re_Ins_Type.adrp_pat)
+    __move_cpat = re.compile(Re_Ins_Type.move_pat)
     #编译操作数
-    __operand_branch_access_cpat = re.compile(operand_type.operand_branch_access_pat)
-    __operand_adrp_access_cpat = re.compile(operand_type.operand_adrp_access_pat)
+    __operand_branch_access_cpat = re.compile(RE_Operand_Type.operand_branch_access_pat)
+    __operand_adrp_access_cpat = re.compile(RE_Operand_Type.operand_adrp_access_pat)
+
+    __ls_bracket_cpat = re.compile(RE_Operand_Type.ls_bracket_pat)
+    __ls_bracketUpdate_cpat = re.compile(RE_Operand_Type.ls_bracketUpdate_pat)
+    __ls_bracket_sp_cpat = re.compile(RE_Operand_Type.ls_bracket_sp_pat)
+    __ls_bracket_reg_cpat = re.compile(RE_Operand_Type.ls_bracket_reg_pat)
+    __ls_sp_cpat = re.compile(RE_Operand_Type.ls_sp_pat)
+    __ls_immOffset_cpat = re.compile(RE_Operand_Type.ls_immOffset_pat)
+    __ls_reg_cpat = re.compile(RE_Operand_Type.ls_reg_pat)
+    __ls_shift_cpat = re.compile(RE_Operand_Type.ls_shift_pat)
+
+    __ls_immeOffset_cpat = re.compile(RE_Operand_Type.immeOffset_pat)
+    __ls_regOffset_cpat = re.compile(RE_Operand_Type.ls_regOffset_pat)
+    __ls_regShift_cpat = re.compile(RE_Operand_Type.ls_regShift_pat)
+    __ls_immeBef_cpat = re.compile(RE_Operand_Type.ls_immeBef_pat)
+    __ls_regBef_cpat = re.compile(RE_Operand_Type.ls_regBef_pat)
+    __ls_regShiftBef_cpat = re.compile(RE_Operand_Type.ls_regShiftBef_pat)
     
 
 
     def __init__(self,statment_dtl,line):
 
+        self.dtl = statment_dtl
         self.addr = statment_dtl[0]
         self.opcode = statment_dtl[1]
         self.ins = statment_dtl[2]
-        self.dtl = statment_dtl
         self.stat_line = line
+       
+        self.operand_num = 0
+        self.type = None
+        self.label = "good"
 
-        self.is_other = False
-
-        self.is_branch = False
         self.branch_addr = None
         self.branch_label = None
         
-        self.__is_load = False
-        self.__is_store = False      
-        self.__ls_reg = None
-        self.__ls_addrMode = None
-        
-        self.is_adrp = False    
+        self.is_lsp = False
+        self.is_sp = False      
+        self.ls_reg = None
+        self.ls_addrMode = None
+           
         self.adrp_addr = None
         self.adrp_label = None
         
-        self.is_move = False
         self.move_from = None
         self.move_to = None
 
         self.ins_type()
+        self.instr_operand_num()
     
     def ins_type(self):
         
@@ -77,35 +131,66 @@ class isa:
         
         if re_branch:
             self.is_branch = True
+            self.type = InsType.branch
             self.__branch_proc()
         elif re_load:
-            self.__is_load = True
+            self.is_load = True
+            self.type = InsType.load
             self.__ls_proc()
         elif re_store:
-            self.__is_store = True
+            self.is_store = True
+            self.type = InsType.store
             self.__ls_proc()
         elif re_adrp:
             self.is_adrp = True
+            self.type = InsType.adr
             self.__adrp_proc()
         elif re_move:
             self.is_move = True
+            self.type = InsType.mov
             print(self.is_move)
             self.__move_proc()
         else:
             self.is_other = True
+            self.type = InsType.other
     
+    def instr_operand_num(self):
+        
+        if self.dtl[3] == None:
+            self.operand_num = 0
+        elif  self.dtl[4] == None:
+            self.operand_num = 1
+        elif  self.dtl[5] == None:
+            self.operand_num = 2
+        elif  self.dtl[6] == None:
+            self.operand_num = 3
+
+
     def __branch_proc(self):
         re_branch_addr = re.match(self.__operand_branch_access_cpat,self.dtl[3])
         temp = re_branch_addr.groups()
         self.branch_addr = temp[0]
         self.branch_label = temp[1]
+
+
     def __ls_proc(self):
-        pass
+        re_lsp = re.match(self.__lsp_cpat,self.ins)
+
+        if re_lsp:
+            self.is_lsp = True
+        else:
+            pass
+    
+
+
+    
     def __adrp_proc(self):
         re_adrp_addr = re.match(self.__operand_adrp_access_cpat,self.dtl[4])
         temp = re_adrp_addr.groups()
         self.adrp_addr = temp[0]
         self.adrp_label = temp[1]
+
+
 
     def __move_proc(self):
         self.move_to = self.dtl[3]
