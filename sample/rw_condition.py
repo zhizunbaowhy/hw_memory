5599937db532ae87bc40737fbce074586459084b
Pigeon.GuGuJi <Pigeon.GuGuJi@outlook.com>
2023-03-26 18:13:20 +0800

    处理读写特征

---
+++ sample/rw_condition.py
@@ -0,0 +1,42 @@
+import queue
+
+class CFG_Node:
+    def __init__(self,nodeObj):
+        node_from = None
+        node_to = None
+        loop_from = None
+        loop_to = None
+
+        stat_rang = None
+        re_value = 0
+        loop_bound = None
+
+        is_from = False#说明入边统计完了，出边那边可以放行了
+        is_to = False#说明出边统计完了
+        is_loop = False
+
+class RW_Model:
+    def __init__(self,cfgObj):
+        self.tor_value = 0
+        self.intor_value = 0
+        # 将cfg_node简化成我们想要的
+        # 给每个节点把包括：从哪儿来，到哪儿去，有无loop
+
+
+
+    def backtrace():
+        #
+        pass
+
+    def find_target_load_or_store(self,start,end,last_idx,target):
+        
+        # while
+        # 
+        #   idx += 1
+        #   if
+        #       
+        #       self.append[]
+        #       
+        #       break
+        
+        pass
