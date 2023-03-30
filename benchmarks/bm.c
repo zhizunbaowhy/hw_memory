28467a33c916cc5b6316b4a4427d44f696c751f5
Pigeon.GuGuJi <Pigeon.GuGuJi@outlook.com>
2023-03-30 21:11:13 +0800

    Dreader

---
+++ benchmarks/bm.c
@@ -0,0 +1,21 @@
+
+
+unsigned long long fib_A[100];
+unsigned long long fib_B[100];
+void fib(){
+    // do fib on fib_A.
+    // do fib on fib_B.
+}
+
+void dijkstra();
+
+
+int main() {
+
+
+    fib();
+    dijkstra();
+    // ...
+
+    return 0;
+}
