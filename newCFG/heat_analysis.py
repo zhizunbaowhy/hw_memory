ff43a37c3fda526228aef18f1e299ce4adfd8f21
刘金源 <11691240+liu-jinyuan111@user.noreply.gitee.com>
2023-04-05 10:56:46 +0000

    update newCFG/heat_analysis.py.
    Signed-off-by: 刘金源 <11691240+liu-jinyuan111@user.noreply.gitee.com>

--- newCFG/heat_analysis.py
+++ newCFG/heat_analysis.py
@@ -2,24 +2,21 @@ class loop_heat:
     def __init__(self,TCFG,LSProc,segmentReader) -> None:
         self.lsproc = LSProc
         self.lds_table = self.lsproc.ls_table
-        # self.a=segmentReader.getbss()
-        # self.b=segmentReader.getdata()
         self.D=segmentReader.getbss()+segmentReader.getdata()
         self.tcfg=TCFG
+        self.addr_bool={i.final_addr : i.ins.is_data_group for i in self.lds_table}
         self.addr_length={i.final_addr : i.ins.ls_data_width for i in self.lds_table}
-
+        self.page_size=4096
         ###以下变量用来承接结果
-
+        self.heat_result=list() 
+        self.lobounds=dict()
     def find_range(self,addr:int):
-        if self.addr_length[addr]!=-1:
-            return addr+int(self.addr_length[addr]/8)
-        for i in self.a:
-            if int(addr)==i[2]:
-                return i[3]
-        for i in self.b:
-            if int(addr)==i[2]:
-                return i[3]
+        for a in self.D:
+            if a[2]<= addr <=a[3]:
+                return a[2],a[3]
+
             
+                  
     def loop_heat_analysis(self):
         self.D[-1][3] = self.D[0][2]
         for l in self.tcfg.loops:
@@ -27,34 +24,54 @@ class loop_heat:
             l.back_edge.is_backEdge = True
         for n in self.tcfg.all_nodes:
             temp = n.inside_loop.name if n.inside_loop is not None else 'None'
-            print("{} {}".format(n.name, temp))
+            #print("{} {}".format(n.name, temp))
         #将对应的指令添加到node的热度信息表中
         for i in self.lds_table:
-            if i.ins.name[0:1] == "l":
+            if i.ins.name[0:1] == "l" and i.is_sp==False:
                 #print(i.ins.tokens)
                 i.node.loadlist.append((i.ins,i.final_addr))
-            elif i.ins.name[0:1] == "s":
+            # elif i.ins.name[0:1] == "l" and i.ins.is_data_group == True:
+            #     pass
+            elif i.ins.name[0:1] == "s" and i.is_sp==False:
                 i.node.storelist.append((i.ins,i.final_addr))
 
-        for node in self.tcfg.all_nodes:
-            for i in node.loadlist:
-                node.heat_ld_result[i[1],self.find_range(i[1])]=0
-            for i in node.loadlist:
-                node.heat_ld_result[i[1],self.find_range(i[1])]+=1
-            for i in node.storelist:
-                node.heat_st_result[i[1],self.find_range(i[1])]=0
-            for i in node.storelist:
-                node.heat_st_result[i[1],self.find_range(i[1])]+=1
 
         for node in self.tcfg.all_nodes:
-            print(node.name)
-            print('load',node.heat_ld_result)
-            print('store',node.heat_st_result)
+            for i in node.loadlist:
+                if self.addr_bool[i[1]]==False:
+                    node.heat_ld_result[i[1],i[1]+int(self.addr_length[i[1]]/8)]=0
+                elif self.addr_bool[i[1]]==True:
+                    a,b=self.find_range(i[1])
+                    for j in range(a,b,4):
+                        node.heat_ld_result[j,j+4]=0
+            for i in node.loadlist:
+                if self.addr_bool[i[1]]==False:
+                    node.heat_ld_result[i[1],i[1]+int(self.addr_length[i[1]]/8)]+=1
+                elif self.addr_bool[i[1]]==True:
+                    a,b=self.find_range(i[1])
+                    for j in range(a,b,4):
+                        node.heat_ld_result[j,j+4]+=1
+            for i in node.storelist:
+                if self.addr_bool[i[1]]==False:
+                    node.heat_st_result[i[1],i[1]+int(self.addr_length[i[1]]/8)]=0
+                elif self.addr_bool[i[1]]==True:
+                    a,b=self.find_range(i[1])
+                    for j in range(a,b,4):
+                        node.heat_st_result[j,j+4]=0  
+            for i in node.storelist:
+                if self.addr_bool[i[1]]==False:
+                    node.heat_st_result[i[1],i[1]+int(self.addr_length[i[1]]/8)]+=1
+                elif self.addr_bool[i[1]]==True:
+                    a,b=self.find_range(i[1])
+                    for j in range(a,b,4):
+                        node.heat_st_result[j,j+4]+=1
+            #print(node.name,'####',node.heat_ld_result,'######',node.heat_st_result)
+
         for l in self.tcfg.loops:
-            print("{} (in {} children {}): {}->{} {}".format(l.name, l.father.name if l.father is not None else "None", [sub_l.name for sub_l in l.children], l.back_edge.src.name, l.back_edge.dst.name, [n.name for n in l.all_nodes]))
+            #print("{} (in {} children {}): {}->{} {}".format(l.name, l.father.name if l.father is not None else "None", [sub_l.name for sub_l in l.children], l.back_edge.src.name, l.back_edge.dst.name, [n.name for n in l.all_nodes]))
             l.back_edge.is_backEdge = True
             for j in l.all_nodes:
-                print(j.name,j.heat_ld_result,j.heat_st_result)
+                #print(j.name,j.heat_ld_result,j.heat_st_result)
                 for key,value in j.heat_ld_result.items():
                     if key in l.loop_ld_heat:
                         l.loop_ld_heat[key]+=value
@@ -65,12 +82,40 @@ class loop_heat:
                         l.loop_st_heat[key]+=value
                     else:
                         l.loop_st_heat[key]=value
-            
+        for l in self.tcfg.loops:    #这里我要把st和sd的合起来，统计一个整体的lds结果
             print(l.name,'ld_heat_analysis',l.loop_ld_heat,'st_heat_analysis',l.loop_st_heat)
-
-
-    def UI_frontend(self):
-        """这个函数用来把现有的各个range的热度归并成一个页面上的热度 用lds的次数来定义"""
-        pass
-
-         
+        for l in self.tcfg.loops:    #这里我要把st和sd的合起来，统计一个整体的lds结果
+            #print(l.name)
+            for key,value in l.loop_ld_heat.items():
+                a=self.find_page(key[0],key[1],value)
+                for i in a:
+                    if i[0] not in l.page_heat_result:
+                        l.page_heat_result[i[0]]=i[1]
+                    else:
+                        l.page_heat_result[i[0]]+=i[1]
+            for key,value in l.loop_st_heat.items():
+                b=self.find_page(key[0],key[1],value)
+                for i in b:
+                    if i[0] not in l.page_heat_result:
+                        l.page_heat_result[i[0]]=i[1]
+                    else:
+                        l.page_heat_result[i[0]]+=i[1]
+            self.heat_result.append([l.name]+[l.page_heat_result])
+        print(self.heat_result)
+        for l in self.tcfg.loops:
+            self.lobounds[l.name]=l.bound
+        for l in self.heat_result:
+            for key in l[1].keys():
+                l[1][key]=l[1][key]*self.lobounds[l[0]]
+    def show(self):
+        print('这是最终的结果')
+        print(self.heat_result) 
+        print(self.lobounds)
+    def find_page(self,pagestart,pageend,figure):  #这个函数用来确认这个range访问了哪个页面
+        temp=list()   
+        if pagestart // self.page_size==pageend // self.page_size:
+            temp.append([pagestart//self.page_size,figure])
+        else:
+            for i in range(pagestart,pageend):
+                temp.append([i //self.page_size,figure])
+        return temp
